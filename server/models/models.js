1f23fb069b19147762d06a827d53a9ce6f3ec7b7
Ben Ling <belinghy@gmail.com>
2018-03-22 19:10:47 -0700

    Remove unused files

--- server/models/models.js
+++
@@ -1,5 +0,0 @@
-var connection = require('../configs/sequelize.js');
-
-var UserMeta = require('./users');
-var Users = connection.define('users', UserMeta.attributes, UserMeta.options);
-module.exports.Users = Users;
