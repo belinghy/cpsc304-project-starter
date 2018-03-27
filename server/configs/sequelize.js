45f985ba6b16ee5280b9f75ee0e10cb107a8dd9b
Chris Liu <y9t0b@ugrad.cs.ubc.ca>
2018-03-26 18:59:05 -0700

    sequlize.js change

--- server/configs/sequelize.js
+++ server/configs/sequelize.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "Demo",
-  process.env.DB_USER || "postgres",
-  process.env.DB_PASSWORD || "postgres",
+  process.env.DB_NAME || "ouikdqby",
+  process.env.DB_USER || "ouikdqby",
+  process.env.DB_PASSWORD || "kSZ2Fq36lvLix8XgOz_2nB8NfofO47ZO",
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "baasu.db.elephantsql.com",
     port: parseInt(process.env.DB_PORT) || 5432,
     dialect: "postgres",
     pool: {
