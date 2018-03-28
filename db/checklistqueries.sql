5a7ca23495bd29b0cab21362c60b6a56a7f3f93f
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2018-03-27 18:34:11 -0700

    added queries
    added some queries

--- db/checklistqueries.sql
+++ db/checklistqueries.sql
@@ -13,14 +13,30 @@ SELECT + attribute
 + WHERE teamsInLeague.tName = + "'" + tName + "'",
 
 -- TODO Division query
+-- Find the names of players who have more than 10 goals
 
-test edit 2
+SELECT name
+FROM playersInTeam P
+WHERE NOT EXISTS (SELECT C.name
+  FROM Class C
+  WHERE NOT EXISTS (SELECT E.snum
+    FROM Enrolled E
+    WHERE C.name=E.cname
+    AND E.snum=S.snum));
 
 
 -- TODO Aggregation query
+-- Find the names of the player(s) who have scored the most goals
 
 SELECT name
 FROM playersInTeam
-WHERE goals = (SELECT max("+ var +") from playersInTeam)
+WHERE goals = (SELECT max(goals) from playersInTeam)
 
 -- TODO Nested aggregation with group-by
+-- Find the number of players on each team
+
+SELECT tName, COUNT(*)
+FROM teamsInLeague
+JOIN playersInTeam ON teamsInLeague.teamID=playersInTeam.teamID
+GROUP BY tName
+ORDER BY COUNT(*) DESC;
