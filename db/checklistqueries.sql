18430ad72547cc76156e1ed5f37741711542b6df
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2018-03-29 16:23:02 -0700

    changed to snake

--- db/checklistqueries.sql
+++ db/checklistqueries.sql
@@ -1,93 +1,93 @@
 -- TODO Selection and Projection Query
 -- Find the team name and standing of teams with 10 red cards
 
-SELECT tname, standing
-FROM teamsInLeague
-WHERE totalRedCards = 10;
+SELECT t_name, standing
+FROM teams_in_league
+WHERE total_red_cardsedCards = 10;
 
 -- Find the team name and standing of teams with 5 yellow cards
 
-SELECT tname, standing
-FROM teamsInLeague
-WHERE totalYellowCards = 5;
+SELECT t_name, standing
+FROM teams_in_league
+WHERE total_yellow_cardsellowCards = 5;
 
 
 -- TODO Join Query
 -- Find the names and corresponding teams of all players
 
-SELECT pName, tName
-FROM playersInTeam P, teamsInLeague T
-WHERE P.teamId = T.teamId;
+SELECT p_name, t_name
+FROM players_in_team P, teams_in_league T
+WHERE P.team_id = T.team_id;
 
 
 -- TODO Division query
 -- Find the names of players who have more than 10 goals
 
-SELECT name
-FROM playersInTeam P
+SELECT p_name
+FROM players_in_team P
 WHERE NOT EXISTS (
-  SELECT P.name
-  FROM playersInTeam P
+  SELECT P.p_name
+  FROM players_in_team P
   WHERE P.goals <= 10);
 
 -- Find the names of players who have less than 10 goals
-  SELECT name
-  FROM playersInTeam P
+  SELECT p_name
+  FROM players_in_team P
   WHERE NOT EXISTS (
-    SELECT P.name
-    FROM playersInTeam P
+    SELECT P.p_name
+    FROM players_in_team P
     WHERE P.goals >= 10);
 
 
 -- TODO Aggregation query
 -- Find the names of the player(s) who have scored the most goals
 
-SELECT name
-FROM playersInTeam
-WHERE goals = (SELECT max(goals) from playersInTeam);
+SELECT p_name
+FROM players_in_team
+WHERE goals = (SELECT max(goals) from players_in_team);
 
 -- Find the names of the player(s) who have scored the least goals
-SELECT name
-FROM playersInTeam
-WHERE goals = (SELECT min(goals) from playersInTeam);
+SELECT p_name
+FROM players_in_team
+WHERE goals = (SELECT min(goals) from players_in_team);
 
 
 -- TODO Nested aggregation with group-by
 -- Find the team with the highest avergae goals scored per player
-select max(AveragesByTeam.avgGoals)
+select max(averages_by_team.avg_goals)
 from (
-    select AVG(goals) as avgGoals
-    from playersInTeam
-    group by teamId
-  )AveragesByTeam;
+    select AVG(goals) as avg_goals
+    from players_in_team
+    group by team_id
+  )averages_by_team;
 
 
 -- Find the number of players on each team
-SELECT tName, COUNT(*)
-FROM teamsInLeague
-JOIN playersInTeam ON teamsInLeague.teamID=playersInTeam.teamID
-GROUP BY tName
+SELECT t_name, COUNT(*)
+FROM teams_in_league
+JOIN players_in_team ON teams_in_league.team_id=players_in_team.team_id
+GROUP BY t_name
 ORDER BY COUNT(*) DESC;
 
 
 
 -- TODO  Delete operation
--- Cascading: Delete a team in a league (teamsInLeague)
--- If you delete a team in a league (teamsInLeague), it follows that all the playes on the team must also be deleted,
+-- Cascading: Delete a team in a league (teams_in_league)
+-- If you delete a team in a league (teams_in_league), it follows that all the playes on the team must also be deleted,
 -- since a player needs to play for a team
 -- This is handled in the creation of the tables
-DELETE FROM teamsInLeague
-WHERE tName = someRandomTeam;
+DELETE FROM teams_in_league
+WHERE t_name = someRandomTeam;
 
--- Non-Cascading: Delete a player on a team (playersInTeam)
--- If you delete a player on a team (playersInTeam), there are no tables that need to be updated
-DELETE FROM playersInTeam
-WHERE Jersey# = 10;
+-- Non-Cascading: Delete a player on a team (players_in_team)
+-- If you delete a player on a team (players_in_team), there are no tables that need to be updated
+DELETE FROM players_in_team
+WHERE jersey_# = 10;
 
 
 -- TODO  Update operation:
 -- Change the jersey number of the highest goal-scorer to number 10
 
-UPDATE playersInTeam
-SET Jersey# = 10
-WHERE goals = (SELECT min(goals) from playersInTeam);
+UPDATE players_in_team
+SET jersey_# = 10
+WHERE goals = (SELECT min(goals) from players_in_team);
