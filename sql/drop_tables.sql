68b202a8d9f6854b45ec85eeae6c18da6119976d
Claire Q. Tan <taborriforever@gmail.com>
2018-11-06 14:33:10 -0800

    update drop_tables

--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,5 +1,16 @@
-drop table license;
-drop table exam;
-drop table branch;
-drop table driver;
+drop table Account;
+drop table Food;
+drop table Owner;
+drop table Restaurant;
+drop table GuestUser;
+drop table SignedUpUser;
+drop table HoursOfOperation;
+drop table Location;
+drop table UserLikesFoodAtRestaurant;
+drop table SignedUpUserFoodSearches;
+drop table SignedUpUserLocationTimeSearches;
+drop table SignedUpUserRestaurantFavourites;
+drop table RestaurantHoursOfOperation;
+drop table FoodServedAtRestaurants;
+
 commit;
