51fe459e3808db7c60929e3d5f6b75806be3c960
Ben Zvikler <bzvikler@gmail.com>
2018-11-09 11:59:10 -0800

    More work on entities

--- breakfast-buddy-api-schema.js
+++ breakfast-buddy-api-schema.js
@@ -1,15 +1,37 @@
 const ENTITIES = {
     user: {
-        id: '123',
+        id: 123,
         name: 'rojin',
     },
     userProfile: {
-
+        userId: 123,
+        userName: 'rojin',
+        userPassword: 'password',
+        profilePicUrl: 'https://google.com/image',
+        favRestaurants: [ ENTITIES.restaurantListItem ],
+        // empty at first
+        favFoods: [ ENTITIES.favFoodListItem ],
+        // empty at first
+        searches: [ ENTITIES.searchHistoryListItem ],
     },
-    owner: {
-
+    ownerProfile: {
+        userId: 123,
+        userName: 'rojin',
+        userPassword: 'password',
+        profilePicUrl: 'https://google.com/image',
+        ownedRestaurants: [ ENTITIES.restaurantListItem ]
     },
     restaurantListItem: {
+        restaurantId: 123,
+        name: 'dons bar and grill',
+        isFavourited: true,
+    },
+    favFoodListItem: {
+        restaurantId: 123,
+        restaurantName: 'ihop',
+        foodType: 'pancakes'
+    },
+    searchHistoryListItem: {
 
     },
     expandedRestaurant: {
